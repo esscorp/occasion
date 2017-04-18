7ff7ea135a63243b1392833b8456e0da81e320c3
Dan Hollenbeck <dan@hollenbecks.com>
2017-04-18 14:16:57 -0500

    interval add and sub

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var Moment = require('moment-timezone');
 var Prove = require('provejs-params');
 // var tzIn = 'US/Central';
 // var tzOut = 'UTC';
-var tzFormat = 'YYYY-MM-DD HH:mm:ss';
+var fmt = 'YYYY-MM-DD HH:mm:ss';
 
 // convert an english date to iso date string
 exports.mdy2iso = function(dateStr) {
@@ -33,7 +33,7 @@ exports.startOfDay = function(dateStr, tzIn, tzOut) {
 		.tz(dateStr, tzIn)
 		.startOf('day')
 		.tz(tzOut)
-		.format(tzFormat);
+		.format(fmt);
 	return moment;
 };
 
@@ -46,7 +46,7 @@ exports.endOfDay = function(dateStr, tzIn, tzOut) {
 		.tz(dateStr, tzIn)
 		.endOf('day')
 		.tz(tzOut)
-		.format(tzFormat);
+		.format(fmt);
 	return moment;
 };
 
@@ -57,19 +57,91 @@ exports.startOfMonthClamped = function(str, range, tzIn, tzOut) {
 
 	var now = Moment.tz(str, tzIn);
 	var day = now.date();
+	var lower = 1 + range;
+	var upper = 31 - range;
 
-	if (day < range) {
+	if (day <= lower) {
+		// late
 		return now
 		.startOf('month')
 		.startOf('day')
 		.tz(tzOut)
-		.format(tzFormat);
-	} else {
+		.format(fmt);
+	} else if (day >= upper) {
+		// early
 		return now
 		.add(1, 'month')
 		.startOf('month')
 		.startOf('day')
 		.tz(tzOut)
-		.format(tzFormat);
+		.format(fmt);
+	} else {
+		// not early or late so assume
+		return now
+		.tz(tzOut)
+		.format(fmt);
+	}
+};
+
+// split interval into expr and unit
+exports.interval = function(str) {
+
+	Prove('S', arguments);
+
+	var parts = str.split(' ');
+	var sign, expr, unit;
+
+	if (parts.length < 2) {
+		return false;
+	} else if (parts.length === 2) {
+		sign = '+';
+		expr = parts[0];
+		unit = parts[1];
+	} else if (parts.length >= 3) {
+		sign = parts[0];
+		expr = parts[1];
+		unit = parts[2];
+	}
+
+	return {
+		sign: sign,
+		expr: expr,
+		unit: unit
+	};
+};
+
+exports.addInterval = function(dateStr, intervalStr) {
+
+	Prove('SS', arguments);
+
+	var moment = Moment.tz(dateStr);
+	var interval = exports.inverval(intervalStr);
+
+	if (interval.sign === '+') {
+		return moment
+		.add(interval.expr, interval.unit)
+		.format(fmt);
+	} else {
+		return moment
+		.sub(interval.expr, interval.unit)
+		.format(fmt);
+	}
+};
+
+exports.subInterval = function(dateStr, intervalStr) {
+
+	Prove('SS', arguments);
+
+	var moment = Moment.tz(dateStr);
+	var interval = exports.inverval(intervalStr);
+
+	if (interval.sign === '+') {
+		return moment
+		.sub(interval.expr, interval.unit)
+		.format(fmt);
+	} else {
+		return moment
+		.add(interval.expr, interval.unit)
+		.format(fmt);
 	}
 };
