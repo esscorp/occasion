163e0a9d911b74afd73cca87dad62b2604feb291
Dan Hollenbeck <dan@hollenbecks.com>
2017-04-19 12:57:25 -0500

    working tests

--- index.js
+++ index.js
@@ -21,38 +21,38 @@ exports.toIsoString = function(dateStr) {
 	dateStr = year + '-' + month + '-' + day;
 	return dateStr;
 };
+//
+// // convert date string to start of day string
+// exports.startOfDay = function(dateStr, tzIn, tzOut) {
+//
+// 	// validate & set defaults
+// 	if (!dateStr) return '';
+// 	if (typeof tzIn !== 'string') tzIn = 'UTC';
+// 	if (typeof tzOut !== 'string') tzOut = 'UTC';
+//
+// 	var moment = Moment
+// 		.tz(dateStr, tzIn)
+// 		.startOf('day')
+// 		.tz(tzOut)
+// 		.format(format);
+// 	return moment;
+// };
 
-// convert date string to start of day string
-exports.startOfDay = function(dateStr, tzIn, tzOut) {
-
-	// validate & set defaults
-	if (!dateStr) return '';
-	if (typeof tzIn !== 'string') tzIn = 'UTC';
-	if (typeof tzOut !== 'string') tzOut = 'UTC';
-
-	var moment = Moment
-		.tz(dateStr, tzIn)
-		.startOf('day')
-		.tz(tzOut)
-		.format(format);
-	return moment;
-};
-
-// convert date string to end of day string
-exports.endOfDay = function(dateStr, tzIn, tzOut) {
-
-	// validate & set defaults
-	if (!dateStr) return '';
-	if (typeof tzIn !== 'string') tzIn = 'UTC';
-	if (typeof tzOut !== 'string') tzOut = 'UTC';
-
-	var moment = Moment
-		.tz(dateStr, tzIn)
-		.endOf('day')
-		.tz(tzOut)
-		.format(format);
-	return moment;
-};
+// // convert date string to end of day string
+// exports.endOfDay = function(dateStr, tzIn, tzOut) {
+//
+// 	// validate & set defaults
+// 	if (!dateStr) return '';
+// 	if (typeof tzIn !== 'string') tzIn = 'UTC';
+// 	if (typeof tzOut !== 'string') tzOut = 'UTC';
+//
+// 	var moment = Moment
+// 		.tz(dateStr, tzIn)
+// 		.endOf('day')
+// 		.tz(tzOut)
+// 		.format(format);
+// 	return moment;
+// };
 
 // split interval into expr and unit
 exports.interval = function(str) {
@@ -100,7 +100,7 @@ exports.convert = function(date, tzFrom, tzTo, fmt) {
 
 /**
  * Calculate audit opened timestamp.
- * @param {String} or {Date}  timestamp string in iso format in client's timezone.
+ * @param {String} timestamp string in iso format in client's timezone.
  * @param {Number} positive integer range around the beginning of month to clamp to.
  * @param {String} client's timezone name.
  * @return {String} timestamp in UTC indicating when audit should be opened.
@@ -108,7 +108,7 @@ exports.convert = function(date, tzFrom, tzTo, fmt) {
  */
 exports.auditOpenedClamped = function(date, range, tz) {
 
-	Prove('*NS', arguments);
+	Prove('SNS', arguments);
 
 	var now = Moment.tz(date, tz);
 	var day = now.date();
@@ -143,7 +143,7 @@ exports.auditOpenedClamped = function(date, range, tz) {
 
 /**
  * Calculate audit opened timestamp.
- * @param {String} or {Date}  timestamp string in iso format in client's timezone.
+ * @param {String} timestamp string in iso format in client's timezone.
  * @param {String} client's timezone name.
  * @return {String} timestamp in UTC indicating when audit should be opened.
  *
@@ -152,14 +152,18 @@ exports.auditOpenedClamped = function(date, range, tz) {
  * UTC timestamp to be saved in the database.
  */
 exports.auditOpened = function(opened, tz) {
-	Prove('*S', arguments);
-	var ts = exports.startOfDay(opened, tz, 'UTC');
+	Prove('SS', arguments);
+	var ts = Moment
+		.tz(opened, tz)
+		.startOf('day')
+		.tz('UTC') // convert back to UTC
+		.format(format);
 	return ts;
 };
 
 /**
  * Calculate audit closed timestamp.
- * @param {String} or {Date}  audit opened timestamp string in iso format in UTC timezone.
+ * @param {String} audit opened timestamp string in iso format in UTC timezone.
  * @param {String} positive opened interval string from audit type recipe.
  * @param {String} client's timezone name.
  * @return {String} timestamp in UTC indicating when audit should be closed.
@@ -169,7 +173,7 @@ exports.auditOpened = function(opened, tz) {
  */
 exports.auditClosed = function(opened, interval, tz) {
 
-	Prove('*SS', arguments);
+	Prove('SSS', arguments);
 
 	var inval = exports.interval(interval);
 
@@ -177,6 +181,7 @@ exports.auditClosed = function(opened, interval, tz) {
 		.tz(opened, 'UTC')
 		.tz(tz) // convert to client's timezone
 		.add(inval.expr, inval.unit) // calculate close date
+		.subtract(1, 'day') // subtract one day because we are adding one day when call endOf('day')
 		.endOf('day') // audits closed at end of day in client's timezone
 		.tz('UTC') // convert back to UTC
 		.format(format);
@@ -186,17 +191,15 @@ exports.auditClosed = function(opened, interval, tz) {
 
 /**
  * Calculate audit period max timestamp.
- * @param {String} or {Date}  audit closed timestamp string in iso format in UTC timezone.
- * @param {String} positive licensure interval string.
- * @param {String} client's timezone name.
+ * @param {String} audit closed timestamp string in iso format in UTC timezone.
  * @return {String} timestamp in UTC indicating when audit period max should be.
  *
  * Audits have a period max value which indicates when max time certificates date
  * are allowed. Currently, audit period max is simply the same as the audit closes timestamp.
  */
-exports.auditPeriodMax = function(closed, interval, tz) { //eslint-disable-line no-unused-vars
+exports.auditPeriodMax = function(closed) {
 
-	Prove('*SS', arguments);
+	Prove('S', arguments);
 
 	var ts = Moment
 		.tz(closed, 'UTC')
@@ -207,7 +210,7 @@ exports.auditPeriodMax = function(closed, interval, tz) { //eslint-disable-line 
 
 /**
  * Calculate audit period min timestamp.
- * @param {String} or {Date}  audit closed timestamp string in iso format in UTC timezone.
+ * @param {String} audit period_max timestamp string in iso format in UTC timezone.
  * @param {String} positive licensure interval string.
  * @param {String} client's timezone name.
  * @return {String} timestamp in UTC indicating when audit period max should be.
@@ -215,17 +218,30 @@ exports.auditPeriodMax = function(closed, interval, tz) { //eslint-disable-line 
  * Audits have a period min value which indicates when min time certificates date
  * are allowed.
  */
-exports.auditPeriodMin = function(closed, interval, tz) {
+exports.auditPeriodMin = function(period_max, interval, tz) {
 
-	Prove('*SS', arguments);
+	Prove('SSS', arguments);
 
 	var inval = exports.interval(interval);
 
+	// todo: we should be adding one second to move the timestamp to the beginning of
+	// the period before we substract the interval. Other you can have artifacts regarding
+	// the number of days in month of the interval substraction.
+
+	// var ts = Moment
+	// 	.tz(period_max, 'UTC')
+	// 	.tz(tz) // convert to client's timezone
+	// 	.subtract(inval.expr, inval.unit) // calculate min moment
+	// 	.add(1, 'day')  // add one day because we are removing one day when call startOf('day')
+	// 	.startOf('day')
+	// 	.tz('UTC') // convert back to UTC
+	// 	.format(format);
+
 	var ts = Moment
-		.tz(closed, 'UTC')
+		.tz(period_max, 'UTC')
 		.tz(tz) // convert to client's timezone
+		.add(1, 'seconds') // change to start of next day
 		.subtract(inval.expr, inval.unit) // calculate min moment
-		.startOf('day')
 		.tz('UTC') // convert back to UTC
 		.format(format);
 
@@ -234,21 +250,20 @@ exports.auditPeriodMin = function(closed, interval, tz) {
 
 /**
  * Calculate audit carryover max timestamp.
- * @param {String} or {Date} audit period min timestamp string in iso format in UTC timezone.
- * @param {String} positive carryover interval string.
- * @param {String} client's timezone name.
+ * @param {String} audit period min timestamp string in iso format in UTC timezone.
  * @return {String} timestamp in UTC indicating when audit carryover max should be.
  *
  * Audits have an optional carryover period which users are allowed to input certificates
  * completed during previous licensure periods. Currently, audit carryover max is simply
  * the same as the audit closes timestamp.
  */
-exports.auditCarroverMax = function(period_min, interval, tz) { //eslint-disable-line no-unused-vars
+exports.auditCarroverMax = function(period_min) {
 
-	Prove('*SS', arguments);
+	Prove('S', arguments);
 
 	var ts = Moment
 		.tz(period_min, 'UTC')
+		.subtract(1, 'second')
 		.format(format);
 
 	return  ts;
@@ -256,26 +271,28 @@ exports.auditCarroverMax = function(period_min, interval, tz) { //eslint-disable
 
 /**
  * Calculate audit carryover min timestamp.
- * @param {String} or {Date} audit period min timestamp string in iso format in UTC timezone.
+ * @param {String} audit period_min timestamp string in iso format in UTC timezone.
  * @param {String} positive carryover interval string.
  * @param {String} client's timezone name.
  * @return {String} timestamp in UTC indicating when audit carryover max should be.
  *
  * Audits have an optional carryover period which users are allowed to input certificates
- * completed during previous licensure periods. Currently, audit carryover max is simply
- * the same as the audit closes timestamp.
+ * completed during previous licensure periods.
+ *
+ * Note: you must pass in period_min here otherwise you can have artifacts in the
+ * subtract method when the number of days in the month causes problems.
  */
-exports.auditCarroverMax = function(period_min, interval, tz) {
+exports.auditCarroverMin = function(carryover_max, interval, tz) {
 
-	Prove('*SS', arguments);
+	Prove('SSS', arguments);
 
 	var inval = exports.interval(interval);
 
 	var ts = Moment
-		.tz(period_min, 'UTC')
+		.tz(carryover_max, 'UTC')
 		.tz(tz) // convert to client's timezone
+		.add(1, 'seconds') // change to start of next day/month
 		.subtract(inval.expr, inval.unit) // calculate min moment
-		.startOf('day')
 		.tz('UTC') // convert back to UTC
 		.format(format);
 
--- test/index.js
+++ test/index.js
@@ -3,6 +3,9 @@
 var assert = require('assert');
 var Occasion = require('..');
 
+var tz = 'US/Central';
+var range = 7;
+
 describe('Tests', function() {
 	describe('exports.toIsoString()', function() {
 		it('should return empty string when the value is not present', function() {
@@ -16,29 +19,29 @@ describe('Tests', function() {
 		});
 	});
 
-	describe('exports.startOfDay()', function() {
-		it('should return empty string when the value is not present', function() {
-			assert.equal(Occasion.startOfDay(), '');
-		});
-		it('should return 1968-01-01 00:00:00 when the value is 1968-01-01 01:00:00', function() {
-			assert.equal(Occasion.startOfDay('1968-01-01 01:00:00'), '1968-01-01 00:00:00');
-		});
-		it('should return 1968-01-01 06:00:00 when the value is 1968-01-01 00:00:00, US/Central, UTC', function() {
-			assert.equal(Occasion.startOfDay('1968-01-01 00:00:00', 'US/Central', 'UTC'), '1968-01-01 06:00:00');
-		});
-	});
+	// describe('exports.startOfDay()', function() {
+	// 	it('should return empty string when the value is not present', function() {
+	// 		assert.equal(Occasion.startOfDay(), '');
+	// 	});
+	// 	it('should return 1968-01-01 00:00:00 when the value is 1968-01-01 01:00:00', function() {
+	// 		assert.equal(Occasion.startOfDay('1968-01-01 01:00:00'), '1968-01-01 00:00:00');
+	// 	});
+	// 	it('should return 1968-01-01 06:00:00 when the value is 1968-01-01 00:00:00, US/Central, UTC', function() {
+	// 		assert.equal(Occasion.startOfDay('1968-01-01 00:00:00', 'US/Central', 'UTC'), '1968-01-01 06:00:00');
+	// 	});
+	// });
 
-	describe('exports.endOfDay()', function() {
-		it('should return empty string when the value is not present', function() {
-			assert.equal(Occasion.endOfDay(), '');
-		});
-		it('should return 1968-01-01 23:59:59 when the value is 1968-01-01 00:00:00', function() {
-			assert.equal(Occasion.endOfDay('1968-01-01 00:00:00'), '1968-01-01 23:59:59');
-		});
-		it('should return 1968-01-02 05:59:59 when the value is 1968-01-01 00:00:00, US/Central, UTC', function() {
-			assert.equal(Occasion.endOfDay('1968-01-01 00:00:00', 'US/Central', 'UTC'), '1968-01-02 05:59:59');
-		});
-	});
+	// describe('exports.endOfDay()', function() {
+	// 	it('should return empty string when the value is not present', function() {
+	// 		assert.equal(Occasion.endOfDay(), '');
+	// 	});
+	// 	it('should return 1968-01-01 23:59:59 when the value is 1968-01-01 00:00:00', function() {
+	// 		assert.equal(Occasion.endOfDay('1968-01-01 00:00:00'), '1968-01-01 23:59:59');
+	// 	});
+	// 	it('should return 1968-01-02 05:59:59 when the value is 1968-01-01 00:00:00, US/Central, UTC', function() {
+	// 		assert.equal(Occasion.endOfDay('1968-01-01 00:00:00', 'US/Central', 'UTC'), '1968-01-02 05:59:59');
+	// 	});
+	// });
 
 	describe('exports.interval()', function() {
 		it('should return 0 DAY interval when the value is not present', function() {
@@ -59,38 +62,99 @@ describe('Tests', function() {
 	});
 
 	describe('exports.auditOpenedClamped()', function() {
-		var days = 7;
-		var tz = 'US/Central';
-
-		it('should return 2000-01-01 06:00:00 when the value is new Date(2000-01-02)', function() {
-			assert.equal(Occasion.auditOpenedClamped(new Date('2000-01-02'), days, tz), '2000-01-01 06:00:00');
+		it('should return 2000-01-01 06:00:00 when the value is new `2000-01-02`', function() {
+			assert.equal(Occasion.auditOpenedClamped('2000-01-02', range, tz), '2000-01-01 06:00:00');
 		});
 		it('should return 2000-01-01 06:00:00 when the value is new Date(1999-12-30)', function() {
-			assert.equal(Occasion.auditOpenedClamped(new Date('1999-12-30'), days, tz), '2000-01-01 06:00:00');
+			assert.equal(Occasion.auditOpenedClamped('1999-12-30', range, tz), '2000-01-01 06:00:00');
 		});
 	});
 
-	describe('exports.auditOpened()', function() {
+	// describe('exports.auditOpened()', function() {
+	// 	it('should return 2000-01-01 06:00:00 when the value is `2000-01-01`', function() {
+	// 		assert.equal(Occasion.auditOpened('2000-01-01', tz), '2000-01-01 06:00:00');
+	// 	});
+	// 	it('should return 1991-12-30 06:00:00 when the value is `1999-12-30`', function() {
+	// 		assert.equal(Occasion.auditOpened('1991-12-30', tz), '1991-12-30 06:00:00');
+	// 	});
+	// });
+
+	// describe('exports.auditClosed()', function() {
+	// 	it('should return 2000-12-31 05:59:59 when the value is `2000-01-01 06:00:00`, `1 YEAR`', function() {
+	// 		assert.equal(Occasion.auditClosed('2000-01-01 06:00:00', '3 MONTH', tz), '2000-03-31 05:59:59');
+	// 	});
+	// });
+
+	// describe('exports.auditPeriodMax()', function() {
+	// 	it('should return 2000-03-31 05:59:59 when the value is `2000-03-31 05:59:59`', function() {
+	// 		assert.equal(Occasion.auditPeriodMax('2000-03-31 05:59:59'), '2000-03-31 05:59:59');
+	// 	});
+	// });
+
+	// describe('exports.auditPeriodMin()', function() {
+	// 	it('should return 1999-04-01 06:00:00 when the value is `2000-03-31 05:59:59`', function() {
+	// 		assert.equal(Occasion.auditPeriodMin('2000-03-31 05:59:59', '1 YEAR', tz), '1999-04-01 06:00:00');
+	// 	});
+	// });
+
+
+	/*
+	The following tests use some simple intervals to make the calculations easier to reason about.
+	Specifically, we set the open interval and licet interval to one month each. This means the
+	audit period and audit open period overlap. Therefore, we should expect the following:
+
+	1. opened is the start of the month,
+	2. closed is the end of the month,
+	3. opened = period_min
+	*/
+	describe('Audit Recipe: 1 MONTH, Opened 3/1/2017', function() {
+
+		var tz = 'US/Central';
+		var opened = '3/1/2017';
+		var interval_open = '1 MONTHS';
+		var interval_licet = '1 MONTHS';
+		var interval_carryover = '1 MONTHS';
+		var closed, period_max, period_min, carryover_max, carryover_min;
+
+		it('expect opened converted to `2017-03-01`', function() {
+			opened = Occasion.toIsoString(opened);
+			assert.equal(opened, '2017-03-01');
+		});
+		it('expect opened of 2017-03-01 06:00:00 UTC', function() {
+			opened = Occasion.auditOpened(opened, tz);
+			assert.equal(opened, '2017-03-01 06:00:00');
+		});
+
+		it('closes of 2017-04-01 04:59:59 UTC', function() {
+			closed = Occasion.auditClosed(opened, interval_open, tz);
+			assert.equal(closed, '2017-04-01 04:59:59');
+		});
+
+		it('period_max of 2017-04-01 04:59:59 UTC', function() {
+			period_max = Occasion.auditPeriodMax(closed);
+			assert.equal(period_max, '2017-04-01 04:59:59');
+		});
+
+		it('period_min of 2017-03-01 06:00:00 UTC', function() {
+			period_min = Occasion.auditPeriodMin(period_max, interval_licet, tz);
+			assert.equal(period_min, '2017-03-01 06:00:00');
+		});
+
+		it('period_min equals opened', function() {
+			period_min = Occasion.auditPeriodMin(closed, interval_licet, tz);
+			assert.equal(period_min, opened);
+		});
+
+		it('carryover_max of 2017-03-01 05:59:59 UTC (one second before period_max)', function() {
+			carryover_max = Occasion.auditCarroverMax(period_min);
+			assert.equal(carryover_max, '2017-03-01 05:59:59');
+		});
+
+		it('carryover_min of 2017-02-01 06:00:00 UTC', function() {
+			carryover_min = Occasion.auditCarroverMin(carryover_max, interval_carryover, tz);
+			assert.equal(carryover_min, '2017-02-01 06:00:00');
+		});
 
 	});
 
-	describe('exports.auditClosed()', function() {
-
-	});
-
-	describe('exports.auditPeriodMax()', function() {
-
-	});
-
-	describe('exports.auditPeriodMin()', function() {
-
-	});
-
-	describe('exports.auditCarroverMax()', function() {
-
-	});
-
-	describe('exports.auditCarroverMin()', function() {
-
-	});
 });
